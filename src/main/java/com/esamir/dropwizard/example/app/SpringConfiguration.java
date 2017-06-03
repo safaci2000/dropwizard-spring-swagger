17897d71ad799fa28b0f41da91927699446461e3
Samir Faci <samir@esamir.com>
2017-06-02 21:12:12 -0700

    Not working

--- src/main/java/com/esamir/dropwizard/example/app/SpringConfiguration.java
+++ src/main/java/com/esamir/dropwizard/example/app/SpringConfiguration.java
@@ -1,14 +1,45 @@
 package com.esamir.dropwizard.example.app;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
 
 @Configuration
 //@EnableTransactionManagement
+//@ImportXml("classpath:com/company/app/datasource-config.xml")
 @ComponentScan(basePackageClasses = SpringConfiguration.class)
 public class SpringConfiguration {
 
+    @Autowired
+    Environment environment;
 
+
+
+    @Bean
+    DataSource dataSource() {
+        BasicDataSource
+            DataSource ds = new org.apache.commons.dbcp2.BasicDataSource();
+            ds.
+            ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+            ds.setUrl("jdbc:oracle:thin:@firefly:1521:HRP2");
+            ds.setUsername("scott");
+            ds.setPassword("tiger");
+            return ds;
+        /*
+         <property name="url" value="${db.url}" />
+       <property name="driverClassName" value="${db.driver}" />
+       <property name="username" value="${db.username}" />
+       <property name="password" value="${db.password}" />
+         */
+        new BasicDataSource()
+
+    }
 
 
 
